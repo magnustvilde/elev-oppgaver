1a4fa293f91b0542d59e6d3b63fbd4cb74bcd0e1
Magnus Tvilde <magnustvilde@gmail.com>
2023-01-04 11:26:38 +0100

    4. januar - password, file-management

--- repetisjon-creative/password.py
+++ repetisjon-creative/password.py
@@ -8,7 +8,21 @@
 
 
 
-# we must now require an email instead of a username for the login.
+# we must now require an email instead of a username for the login, but 
+# keep the option of the username 
 # the email must have a '@', and end in '.no', '.com' or '.org'
+# still ask the user
 
 
+# the user should be asked to put their first and last-name, and the program 
+# should then use their first-name in all subsequent prompts
+
+
+# put the code into nice functions
+
+
+# we must store the password hashed - use the hashlib-module!
+
+
+
+# next up we are going to store the information in a binary-file
