84ce458f77568d7e91e6ed73b0dd2a6ed530bbbc
Magnus Tvilde <magnustvilde@gmail.com>
2023-01-04 10:23:51 +0100

    Repetisjon Python 2

---
+++ repetisjon-creative/password.py
@@ -0,0 +1,14 @@
+# create a prompt where the user is asked to create an account with a username
+# and a password
+
+
+
+# the password should be between 8 and 16 in length
+# and must contain a number, a lowercase letter and an uppercase letter
+
+
+
+# we must now require an email instead of a username for the login.
+# the email must have a '@', and end in '.no', '.com' or '.org'
+
+
